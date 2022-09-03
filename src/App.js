39343bd7506208a441d111a5505261d1bc174f10
Vaibhav4556 <burdev741@gmail.com>
2022-09-03 11:45:10 +0530

    heroku api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {useEffect, useState} from "react"
+
+// const API = 'http://localhost:4000';
+const API = 'https://vaibhav1app.herokuapp.com';
+
 
 function App() {
+  const [mobiles,setMobiles ]= useState ([]);
+ 
+
+  useEffect(() => {
+     fetch(`${API}/mobiles`)
+      .then(response=> response.json())
+      .then(data => setMobiles(data));
+  }, []);
+
+  //API  http://localhost:4000/mobiles
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="phone-list-container">
+        {mobiles.map((mb) => (
+          <Phone key={mb._id} mobile={mb} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function Phone({ mobile }) {
+  // const mobile = {
+  //   model: "OnePlus 9 5G",
+  //   img: "https://m.media-amazon.com/images/I/61fy+u9uqPL._SX679_.jpg",
+  //   company: "Oneplus",
+  // };
+
+  return (
+    <div className="phone-container">
+      <img className="phone-picture" src={mobile.img} />
+      <h2 className="phone-name">{mobile.model}</h2>
+      <p className="phone-company">{mobile.company}</p>
     </div>
   );
 }
